3d88b7dd2c4facdf74c2da0c357b4023719a6a47
santi <santiagomuller45@gmail.com>
2024-09-17 21:42:41 -0300

    arreglo de css en about us y contact además de la landing agregando estilo bold al texto

--- quimar-front/src/views/Contact/Contact.jsx
+++ quimar-front/src/views/Contact/Contact.jsx
@@ -29,13 +29,14 @@ const Contact = () => {
             <section className={style.contactSection}>
                 {contactDetails.map(({ icon, label, value }, index) => (
                     <h4 key={index} className={style.contactItem}>
-                        <FontAwesomeIcon icon={icon} /> {label}: {value}
+                        <FontAwesomeIcon icon={icon} /> {label}: <strong> {value} </strong>
                     </h4>
                 ))}
             </section>
             <section className={style.header}>
                 <h2 className={style.title}>Nuestros Horarios</h2>
-                <h4 className={style.text}>Lunes a viernes</h4>
+                <br/>
+                <h4 className={style.subTitle}>Lunes a viernes</h4>
                 <h6 className={style.text}>08:00 hs - 17:00hs</h6>
             </section>
         </div>
