90abab4388016ee5cfef4c28dcb34e7c3ba872cb
santimuller45 <santiagomuller45@gmail.com>
2025-09-16 18:51:38 -0300

    Se agrego el Número del pedido en el archivo PDF al imprimir un pedido v2

--- quimar-front/src/components/OrdersUserDetail/generatePDF/generatePDF.js
+++ quimar-front/src/components/OrdersUserDetail/generatePDF/generatePDF.js
@@ -29,9 +29,9 @@ export const generateOrderPDF = (order, user) => {
     doc.setFontSize(12); // Reducir tamaño de fuente para el resto de la información
 
     // N° Cliente y Nombre del Cliente (Izquierda)
-    doc.text(`N° Cliente: ${user.userNumber}`, 20, 60);  // Número del cliente
-    doc.text(`Cliente: ${user.name}`, 20, 70);  // Nombre del cliente
-    doc.text(`Pedido N°: ${order.id}`, 20, 80);  // Id del pedido
+    doc.text(`Pedido N°: ${order.id}`, 20, 60);  // Id del pedido
+    doc.text(`N° Cliente: ${user.userNumber}`, 20, 70);  // Número del cliente
+    doc.text(`Cliente: ${user.name}`, 20, 80);  // Nombre del cliente
 
     // Fecha y Hora (Derecha)
     const formattedDate = `${order.orderDate.day}/${order.orderDate.month}/${order.orderDate.year}`;
